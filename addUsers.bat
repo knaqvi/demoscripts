afe86f49695211f86c31b8cd48ae983dd36d27e1
knaqvi <knaqvi@users.noreply.github.com>
2023-02-16 17:22:41 -0800

    Create addUsers.bat

---
+++ addUsers.bat
@@ -0,0 +1,12 @@
+@echo off
+set /p adminuser=Enter your AD username: 
+set /p password=Enter your AD password: 
+
+net user %adminuser% /domain
+
+dsadd group "CN=Sales,OU=Users,DC=talabs,DC=local"
+dsadd user "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local" -pwd Password2 -mustchpwd yes
+dsadd user "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local" -pwd Password1 -mustchpwd yes
+
+dsmod group "CN=Sales,OU=Users,DC=talabs,DC=local" -addmbr "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local"
+dsmod group "CN=Sales,OU=Users,DC=talabs,DC=local" -addmbr "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local"
