8e517e1c5d79952b13c56147228122a02da10595
knaqvi <knaqvi@users.noreply.github.com>
2023-02-16 17:20:30 -0800

    Update addUsers.ps1

--- addUsers.ps1
+++ addUsers.ps1
@@ -9,9 +9,8 @@ Connect-ADDirectoryServer -Credential $credential
 New-ADGroup -Name "Sales" -GroupCategory Security -GroupScope Global -Path "OU=Groups,DC=domain,DC=com"
 
 # Create new users
-New-ADUser -Name "John Smith" -UserPrincipalName "john.smith@talabs.local" -Path "OU=Users,DC=domain,DC=com" -AccountPassword (ConvertTo-SecureString "Password1" -AsPlainText -Force) -Enabled $true
-New-ADUser -Name "Jane Doe" -UserPrincipalName "jane.doe@talabs.local" -Path "OU=Users,DC=domain,DC=com" -AccountPassword (ConvertTo-SecureString "Password2" -AsPlainText -Force) -Enabled $true
-New-ADUser -Name "Bob Johnson" -UserPrincipalName "bob.johnson@talabs.local" -Path "OU=Users,DC=domain,DC=com" -AccountPassword (ConvertTo-SecureString "Password3" -AsPlainText -Force) -Enabled $true
+New-ADUser -Name "Randy Jurdi" -UserPrincipalName "randy.jurdi@talabs.local" -Path "OU=Users,DC=domain,DC=com" -AccountPassword (ConvertTo-SecureString "Password1" -AsPlainText -Force) -Enabled $true
+New-ADUser -Name "Kashif Naqvi" -UserPrincipalName "kashif.naqvi@talabs.local" -Path "OU=Users,DC=domain,DC=com" -AccountPassword (ConvertTo-SecureString "Password2" -AsPlainText -Force) -Enabled $true
 
 # Add users to group
-Add-ADGroupMember -Identity "Sales" -Members "John Smith", "Jane Doe", "Bob Johnson"
+Add-ADGroupMember -Identity "Sales" -Members "Randy Jurdi", "Kashif Naqvi"
