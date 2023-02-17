56c08a8a34022b176dd08fcbeec40fec8ae863d3
knaqvi <knaqvi@users.noreply.github.com>
2023-02-16 17:59:58 -0800

    Update addUsers.bat

--- addUsers.bat
+++ addUsers.bat
@@ -4,9 +4,9 @@ set /p password=Enter your AD password:
 
 net user %adminuser% /domain
 
-dsadd group "CN=Sales,OU=Users,DC=talabs,DC=local"
-dsadd user "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local" -pwd Password2 -mustchpwd yes
-dsadd user "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local" -pwd Password1 -mustchpwd yes
+dsadd group "CN=Sales,CN=Users,DC=talabs,DC=local"
+dsadd user "CN=Kashif Naqvi,CN=Users,DC=talabs,DC=local" -pwd Password2 -mustchpwd yes
+dsadd user "CN=Randy Jurdi,CN=Users,DC=talabs,DC=local" -pwd Password1 -mustchpwd yes
 
-dsmod group "CN=Sales,OU=Users,DC=talabs,DC=local" -addmbr "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local"
-dsmod group "CN=Sales,OU=Users,DC=talabs,DC=local" -addmbr "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local"
+dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local"
+dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local"
