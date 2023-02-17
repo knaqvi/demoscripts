a5487dba01f5be474ca5a295bdd1bc6fd82f9cac
knaqvi <knaqvi@users.noreply.github.com>
2023-02-17 10:45:47 -0800

    Update setupData.ps1

--- setupData.ps1
+++ setupData.ps1
@@ -4,14 +4,13 @@ $folderName = Read-Host "Enter folder name"
 # Create the folder
 New-Item -ItemType Directory -Path $folderName
 
-# Prompt user for file name
-$fileName = Read-Host "Enter file name (without extension)"
+# Prompt user for file name prefix
+$fileNamePrefix = Read-Host "Enter file name prefix"
 
-# Create 10 files with the specified name and size
+# Create 10 files with 5GB size
 for ($i = 1; $i -le 10; $i++) {
-    $filePath = $folderName + "\" + $fileName + $i.ToString() + ".txt"
-    $stream = New-Object System.IO.FileStream($filePath, [System.IO.FileMode]::Create)
-    $stream.SetLength(5GB)
-    $stream.Close()
-    Write-Host "Created file: " $filePath
+    $fileName = $fileNamePrefix + $i.ToString().PadLeft(2, '0') + ".txt"
+    $filePath = Join-Path $folderName $fileName
+    Write-Host "Creating file $fileName..."
+    fsutil file createnew $filePath 5000000000
 }
