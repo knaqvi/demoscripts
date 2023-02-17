6534334af8e27d4a6a8b47bff6387cb9c317e538
knaqvi <knaqvi@users.noreply.github.com>
2023-02-16 18:02:20 -0800

    Update addUsers.bat

--- addUsers.bat
+++ addUsers.bat
@@ -8,5 +8,5 @@ dsadd group "CN=Sales,CN=Users,DC=talabs,DC=local"
 dsadd user "CN=Kashif Naqvi,CN=Users,DC=talabs,DC=local" -pwd Password2 -mustchpwd yes
 dsadd user "CN=Randy Jurdi,CN=Users,DC=talabs,DC=local" -pwd Password1 -mustchpwd yes
 
-dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Kashif Naqvi,OU=Users,DC=talabs,DC=local"
-dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Randy Jurdi,OU=Users,DC=talabs,DC=local"
+dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Kashif Naqvi,CN=Users,DC=talabs,DC=local"
+dsmod group "CN=Sales,CN=Users,DC=talabs,DC=local" -addmbr "CN=Randy Jurdi,CN=Users,DC=talabs,DC=local"
